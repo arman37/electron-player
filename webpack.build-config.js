ecae7b90e829a45e7a1d57fec8581faffc4db3de
arman37 <arman.it37@gmail.com>
2018-04-27 18:41:47 +0600

    Added seperate webpack build configuration

---
+++ webpack.build-config.js
@@ -0,0 +1,23 @@
+/*
+ ./webpack.build-config.js
+ */
+module.exports = {
+  entry: './app/components/app.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+};
