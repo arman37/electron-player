c91061986113b087bbd79067a7f149526098a877
arman37 <arman.it37@gmail.com>
2018-05-03 19:09:11 +0600

    Access add-library component properties directly from redux store

--- app/components/dumb/add-library.component.js
+++ app/components/dumb/add-library.component.js
@@ -5,46 +5,73 @@
  */
 
 import React from 'react';
+import { connect } from "react-redux";
 import Dialog from 'material-ui/Dialog';
 import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
+import {handleDialogOpen, handleDrawerOpen, handleCatViewChange, toggleTableView, updateMetadata, addPath, clearPath} from '../../actions';
 
+let mapStateToProps = state => ({
+  dialogOpen: state.get('dialogOpen'),
+  searchPathList: state.get('searchPathList')
+});
+
+let mapDispatchToProps = dispatch => ({
+  addPath: (param) => dispatch(addPath(param)),
+  clearPath: (param) => dispatch(clearPath(param)),
+  handleClose: (param) => dispatch(handleDialogOpen(param))
+});
+
+/**
+ *
+ * @param {boolean} dialogOpen
+ * @param {function} handleClose
+ * @param {array} searchPathList
+ * @param {function} addPath
+ * @param {function} clearPath
+ * @returns {XML}
+ * @constructor
+ */
 const AddLibrary = ({dialogOpen, handleClose, searchPathList, addPath, clearPath}) => {
-    const actions = [
-        <FlatButton
-            label="Add Directory"
-            primary={true}
-            keyboardFocused={true}
-            onClick={addPath.bind(null)} />,
-        <FlatButton
-            label="Clear"
-            primary={true}
-            onClick={clearPath.bind(null)} />,
-        <FlatButton
-            label="OK"
-            primary={true}
-            onClick={handleClose.bind(null, false)} />
-    ];
+  const actions = [
+    <FlatButton
+      label="Add Directory"
+      primary={true}
+      keyboardFocused={true}
+      onClick={addPath.bind(null)} />,
+    <FlatButton
+      label="Clear"
+      primary={true}
+      onClick={clearPath.bind(null)} />,
+    <FlatButton
+      label="OK"
+      primary={true}
+      onClick={handleClose.bind(null, false)} />
+  ];
 
-    return (
-        <div className="form-dialog">
-            <Dialog
-                title="Dialog With Actions"
-                actions={actions}
-                modal={false}
-                open={dialogOpen}
-            >
-                <TextField
-                    value={searchPathList.toJS()}
-                    disabled={true}
-                    hintText="Path list to search for music..."
-                    floatingLabelText="Path list to search for music..."
-                    multiLine={true}
-                    rows={5}
-                /><br />
-            </Dialog>
-        </div>
-    )
+  return (
+    <div className="form-dialog">
+      <Dialog
+        title="Dialog With Actions"
+        actions={actions}
+        modal={false}
+        open={dialogOpen} >
+
+        <TextField
+          value={searchPathList.toJS()}
+          disabled={true}
+          hintText="Path list to search for music..."
+          floatingLabelText="Path list to search for music..."
+          multiLine={true}
+          rows={5}
+        /><br />
+
+      </Dialog>
+    </div>
+  )
 };
 
-export default AddLibrary;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AddLibrary);
--- app/components/dumb/main-content.component.js
+++ app/components/dumb/main-content.component.js
@@ -12,44 +12,39 @@ import VideoController from '../dumb/video.component';
 import AddLibrary from '../dumb/add-library.component';
 
 const MainContent = ({tableView, audioView, audioTrackList, addPath, toggleTableView, dialogOpen, drawerOpen, trackMetadata, tbodyHeight, clearPath, searchPathList, backgroundImage, updateMetadata, handleClose, scanningForTracks, handleViewChange, handleDialogOpen, handleDrawerOpen, handleCatViewChange}) => {
-    return (
-        <div className="main__content">
-            <Header />
-            <AddLibrary
-                dialogOpen={dialogOpen}
-                addPath={addPath}
-                clearPath={clearPath}
-                searchPathList={searchPathList}
-                handleClose={handleDialogOpen} />
-            <div className="content__holder">
-                <div className="content__header">
-                    <div className="switch__button__group">
+  return (
+    <div className="main__content">
+      <Header />
+      <AddLibrary />
+      <div className="content__holder">
+        <div className="content__header">
+          <div className="switch__button__group">
 
-                    </div>
-                    <div className="track__details shadow elegant-shadow">
-                        {
-                            trackMetadata ?
-                                (<Details metadata={trackMetadata} />)
-                                :
-                                ('')
-                        }
-                    </div>
-                </div>
-                <div className="content__audio__video" style={{backgroundImage: `url(${backgroundImage ? backgroundImage : (Math.round(Math.random()) === 1 ? 'public/images/fire.jpg' : 'public/images/music-woman.jpg') })`, backgroundSize: 'contain'}}>
-                    {
-                        audioView ?
-                        (<AudioController
-                            tableView={tableView}
-                            tbodyHeight={tbodyHeight}
-                            trackList={audioTrackList}
-                            updateMetadata={updateMetadata} />)
-                        :
-                        (<VideoController/>)
-                    }
-                </div>
-            </div>
+          </div>
+          <div className="track__details shadow elegant-shadow">
+            {
+              trackMetadata ?
+                (<Details metadata={trackMetadata} />)
+                :
+                ('')
+            }
+          </div>
         </div>
-    )
+        <div className="content__audio__video" style={{backgroundImage: `url(${backgroundImage ? backgroundImage : (Math.round(Math.random()) === 1 ? 'public/images/fire.jpg' : 'public/images/music-woman.jpg') })`, backgroundSize: 'contain'}}>
+          {
+            audioView ?
+              (<AudioController
+                tableView={tableView}
+                tbodyHeight={tbodyHeight}
+                trackList={audioTrackList}
+                updateMetadata={updateMetadata} />)
+              :
+              (<VideoController/>)
+          }
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default MainContent;
